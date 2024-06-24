fa08186c724653516937b6a42e298027525d864c
John Hooks <johnhookes@aol.com>
2024-06-24 08:17:57 +0000

    added creat random number function

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+// function for running the game
+// get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+       button.addEventListener("click", function() {
+        if (this.getAttribute("data-type") === "submit") {
+            alert("you clicked submit");
+        }
+        else {
+            let gameType = this.getAttribute("data-type");
+            alert(`you clicked ${gameType}`);
+        }
+       })
+    }
+})
+
+
+function runGame() {
+    let num1 = Math.floor(Math.random()*25) + 1;
+    let num2 = Math.floor(Math.random()*25) + 1;
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+    
+}
+
+function incrementScore() {
+
+}
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
