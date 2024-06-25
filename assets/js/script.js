bddc33d427a7238bf2330af908cd89c97c4111c7
John Hooks <johnhookes@aol.com>
2024-06-25 08:24:58 +0000

    added the division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,6 +49,13 @@ function runGame(gameType) {
     else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
     }
+/**
+ * 
+ * need to finish
+ */
+    else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
+    }
 
     else{
         alert(`Unknown game type: ${gameType}`);
@@ -97,6 +104,14 @@ function calculateCorrectAnswer() {
    else if (operator === "-") {
     return [operand1 - operand2, "subtract"];
    }
+/**
+ * 
+ * 
+ * 
+ */
+   else if (operator === "/") {
+    return [operand1 / operand2, "division"];
+   }
 
     else {
         alert(`Unimplemented operator ${operator}`);
@@ -139,4 +154,11 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById(`operand1`).textContent = operand1;
     document.getElementById(`operand2`).textContent = operand2;
     document.getElementById(`operator`).textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
+    document.getElementById(`operand1`).textContent = operand1;
+    document.getElementById(`operand2`).textContent = operand2;
+    document.getElementById(`operator`).textContent = "/";
 }
